767081208a1588f0f57ad40809adf61c3f1d8728
Jefferson <jefferson.chileno@epn.edu.ec>
2024-10-26 07:13:38 -0500

    inicio de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MainGame from './pages/MainGame'
+
+function App() {
+	return (
+		<>
+			<MainGame />
+		</>
+	)
+}
+
+export default App
---
+++ src/pages/MainGame.jsx
@@ -0,0 +1,26 @@
+import './mainGame.css'
+
+const MainGame = () => {
+	return (
+		<div className='root-game'>
+			<h1 className='titulo-juego'>INFINITY QUALITY</h1>
+			<main>
+				<section className='juego'>AQUI VA EL JUEGO</section>
+				<section className='selector-color'>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+					<span>COLOR 1</span>
+				</section>
+			</main>
+		</div>
+	)
+}
+
+export default MainGame
