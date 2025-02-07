8735b7ea78d1b058a6369aec05daa70202569cf8
Sebastian Guerra <sebastian.guerra01@epn.edu.ec>
2025-02-06 23:11:19 -0500

    docs(general): creación de la rama docs y subida de documentación de la v3

--- src/context/StateFetchContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const StateFetchContext = createContext()
-
-export default StateFetchContext
--- src/hooks/useStateFetch.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-const useStateFetch = () => {
-	const [loading, setLoading] = useState(false)
-
-	return { loading, setLoading }
-}
-
-export default useStateFetch
